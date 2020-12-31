4444dfe0df01bb10970e61a8763b7948160c7cce
anhkhoido <anhkhoido@gmail.com>
2020-12-30 20:35:11 -0500

    Created the infrastructure for vehicles

---
+++ EmergencyTaskForce/src/main/java/com/anhkhoido/game/EmergencyTaskForce/dao/vehicle/VehicleRepository.java
@@ -0,0 +1,7 @@
+package com.anhkhoido.game.EmergencyTaskForce.dao.vehicle;
+
+import com.anhkhoido.game.EmergencyTaskForce.model.vehicle.Vehicle;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VehicleRepository extends CrudRepository<Vehicle, Integer> {
+}
---
+++ EmergencyTaskForce/src/main/java/com/anhkhoido/game/EmergencyTaskForce/model/vehicle/Cruiser.java
@@ -0,0 +1,8 @@
+package com.anhkhoido.game.EmergencyTaskForce.model.vehicle;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Cruiser extends Vehicle {
+
+}
