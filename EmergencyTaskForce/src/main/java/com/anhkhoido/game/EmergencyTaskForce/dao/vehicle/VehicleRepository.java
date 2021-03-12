fb257d7dded2541f1d6504054bc67b8de18c54f9
anhkhoido <anhkhoido@gmail.com>
2021-03-12 11:55:55 -0500

    Removed inheritance

--- EmergencyTaskForce/src/main/java/com/anhkhoido/game/EmergencyTaskForce/dao/vehicle/VehicleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anhkhoido.game.EmergencyTaskForce.dao.vehicle;
-
-import com.anhkhoido.game.EmergencyTaskForce.model.vehicle.Vehicle;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VehicleRepository extends CrudRepository<Vehicle, Integer> {
-}
