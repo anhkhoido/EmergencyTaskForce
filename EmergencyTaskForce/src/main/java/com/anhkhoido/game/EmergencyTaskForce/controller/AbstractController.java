afd62599e1a295d55db80a06065e1faf1343c22d
anhkhoido <anhkhoido@gmail.com>
2021-05-22 14:54:58 -0400

    addConstant - Added one constant in AbstractController.java

--- EmergencyTaskForce/src/main/java/com/anhkhoido/game/EmergencyTaskForce/controller/AbstractController.java
+++ EmergencyTaskForce/src/main/java/com/anhkhoido/game/EmergencyTaskForce/controller/AbstractController.java
@@ -1,20 +1,22 @@
 package com.anhkhoido.game.EmergencyTaskForce.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public abstract class AbstractController<T> {
 
-    @GetMapping(path = "/{id}")
+    private final String URL_PATH_ID = "/{id}";
+
+    @GetMapping(path = URL_PATH_ID)
     public abstract T findById(@PathVariable(value = "id") Integer id);
 
     @GetMapping(path = "/findAll")
     public abstract Iterable<T> findAll();
 
-    @DeleteMapping(path = "/{id}")
+    @DeleteMapping(path = URL_PATH_ID)
     public abstract void deleteById(@PathVariable(value = "id") Integer id);
 
     @DeleteMapping(path = "/deleteAll")
